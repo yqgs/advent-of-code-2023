1a322041ade3ec2a810b0877b1e6e1db13ed763c
yqgs <gregsun1998@gmail.com>
2023-12-01 16:03:22 -0500

    Update 1_1.py
    redid part 1 with regex

--- day 1/1_1.py
+++ day 1/1_1.py
@@ -1,18 +1,11 @@
+import re
+
 sum = 0
 
 with open("day 1/input1_1.txt", "r") as file:
     for line in file:
-        num = 0
-        for char in line:
-            if char.isdigit():
-                num += int(char)
-                break
-        num *= 10
-        rev = reversed(line)
-        for char in rev:
-            if char.isdigit():
-                num += int(char)
-                break
-        sum += num
+        x = re.findall("[0-9]", line)
+        sum += (int(x[0]) * 10) + int(x[-1])
+
 
 print(sum)
